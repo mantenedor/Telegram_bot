3b57921637e7c20ce993678957fdddc7cc2a1603
Mantenedor <wagtonazevedo@gmail.com>
2022-02-14 16:21:56 -0300

    Update oto.sh
    Nova versão

--- oto.sh
+++ oto.sh
@@ -1,74 +1,94 @@
 #!/bin/bash
 
-CONF="/opt/oto/oto.conf"
-TOKEN=`grep "TOKEN" $CONF | cut -d= -f2`
-URL="https://api.telegram.org/bot$TOKEN"
-SCRIPT=`grep "SCRIPT" $CONF | cut -d= -f2`
+API="https://api.telegram.org"
+TOKEN=$TOKEN #Definir variável de ambinete
+URL="$API/bot$TOKEN"
+#BASE="/mnt/data/oto/data"
+JSON="/mnt/data/oto/data/oto.json"
+RESPOSTA="Saldações!"
 
 function GET {
-        echo 'GET'
-        MSG=`curl $URL/getupdates | jq .result[1]`
+
+        FUNCTION="getupdates"
+
+        MSG=`curl $URL/$FUNCTION | jq .result[1]`
+        UPDATEID=`echo $MSG | jq .update_id`
         FROMID=`echo $MSG | jq .message.from.id`
         TYPE=`echo $MSG | jq .message.entities[0].type`
         TXT=`echo $MSG | jq .message.text`
         MSGID=`echo "$MSG" | jq .message.message_id`
-        UPDATEID=`echo $MSG | jq .update_id`
+        USERNAME=`echo $MSG | jq .message.chat.username`
+        TRUSTID=`grep -x "$FROMID" /opt/oto/.trust`
 
-        CHATID=`echo "$MSG" | tail -n1 | cut -d: -f5 | cut -d, -f1`
-
-        echo $FROMID
-        echo $TYPE
-        echo $TXT
-
-        echo "$MSG" > /tmp/.msg
         curl "$URL/getupdates?offset=$UPDATEID"
 }
 
-
 function CHAT {
-        echo "CHAT"
+
+        FUNCTION="sendMessage"
+        curl -X POST "$URL/$FUNCTION" -d "chat_id=$FROMID&text=$RESPOSTA"
+}
+
+function COMAND {
+
+                echo "CHAT"
+
+                if [[ "$TXT" = '"/sleep"' ]];then
+                        RESPOSTA="Eu nunca durmo."
+                        CHAT
+                fi
+
+}
+
+function SEQUENCE {
+
         GET
-        if [[ "$TYPE" = '"hashtag"' ]];then
-                CLI
-                curl -X POST "$URL/sendMessage" -d "chat_id=$FROMID&text=$RESPOSTA"
+
+        if [ "$TRUSTID" != "$FROMID" ]; then
+                if [ "$UPDATEID" != null ];then
+                        SECURITY
+                fi
         else
-                if [[ "$TXT" != null ]];then
-                        FUNC
-                        curl -X POST "$URL/sendMessage" -d "chat_id=$FROMID&text=$RESPOSTA"
+                if [[ "$TYPE" = '"bot_command"' ]];then
+                        echo "COMAND"
+                        BUSCA
+                        #COMAND
+                else
+                        BUSCA
                 fi
         fi
+
 }
 
-function CLI {
-        echo "CLI"
-        CMD=`echo $TXT | sed 's/^"#//' | sed 's/"$//'`
-        RESPOSTA=`$CMD`
+function BUSCA {
+
+        BASE=`cat "$JSON"`
+        TERMO=`echo "$TXT" | tr -d '"' | tr -d '\/'`
+        RESPOSTA=`echo "$BASE" | jq ."$TERMO"`
+
+        if [ "$RESPOSTA" = null ];then
+                RESPOSTA="Não computa."
+        fi
+
+        echo "$TERMO"
+        CHAT
 }
 
-function FUNC {
-        echo "FUNC"
-        RESPOSTA="`echo "$TXT"` não computa."
-        if [[ "$TXT" = '"Olá"' ]];then
-                RESPOSTA="Saldações!"
-        fi
-        if [[ `echo "$TXT" | cut -b 2-6` = 'cofre' ]];then
-                DELIMITADOR=`echo "$TXT" | cut -d' ' -f2`
-                SERVICO=`echo "$TXT" | cut -d' ' -f3`
-                SECRET=`echo $TXT | cut -d' ' -f4 | tr -d '"'`
-                SERVICO=`echo "$SERVICO" | md5sum | tr -d [0-9] | cut -b 1-3`
-                SECRET=`echo "obase=16; $SECRET" | bc`
-                RESPOSTA="$SERVICO$DELIMITADOR$SECRET"
-        fi
+function SECURITY {
 
-
+                RESPOSTA="Tentativa de comunicação desconhecida: $MSG"
+                #echo $RESPOSTA >> /opt/oto/security.log
+                FROMID="757541494"
+                curl -X POST "$URL/sendMessage" -d "chat_id=$FROMID&text=$RESPOSTA"
 }
 
 function RUN {
+
         while true; do
-                CHAT;
-                TIME=`echo $RANDOM % 10 + 1 | bc`
-                sleep $TIME
+                SEQUENCE
+                sleep 3
         done
+
 }
 
 RUN
